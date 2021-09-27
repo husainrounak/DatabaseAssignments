df561701a2afdce7a160f19be5a97545f976ee5a
husainrounak <89070068+husainrounak@users.noreply.github.com>
2021-09-27 17:02:40 +0530

    Add files via upload

---
+++ PHP_ASSIGNMENT/shareboard_assignment/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shareboard");
+
+ define("ROOT_PATH", "/shareboard_assignment/");
+ define("ROOT_URL", "http://localhost/shareboard_assignment/");
+?>
